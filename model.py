3cc63aaf9ad6f21cb91756c8b7ff80c1e5ac56db
53X <mirage_1997@mail.ru>
2018-08-14 01:18:46 +0530

    model built

--- model.py
+++ model.py
@@ -1,9 +1,12 @@
 import keras
-from keras.layers import Conv1D, MaxPooling1D, Dense, Input, Embedding
+from keras.layers import Conv1D, MaxPooling1D, Dense, Input, Embedding, Dropout
+from keras.optimizers import SGD
+
+optimizer = SGD(lr=0.01, momentum = 0.9)
 
 
 
-def character_model(vocab_size=69, maxlen=1024):
+def character_model(vocab_size=69, maxlen=1024, classification):
 
 	input_layer = Input(shape=(maxlen, ), name='sentence input')
 
@@ -33,7 +36,23 @@ def character_model(vocab_size=69, maxlen=1024):
 
 	pool_6 = MaxPooling1D(pool_size=3, padding='valid', name='third pool')(conv_6)
 
+	flattened = Flatten()(pool_6)
 
+	fully_conn_1 = Dense(2048, activation='relu')(flattened)
+
+	fully_conn_1 = Dropout(0.5)(fully_conn_1)
+
+	fully_conn_2 = Dense(2048, activation='relu')(fully_conn_1)
+
+	fully_conn_2 = Dropout(0.5)(fully_conn_2)
+
+	output = Dense(classification, activation='softmax')(fully_conn_2)
+
+	model = Model(inputs = input_layer, outputs = output)
+
+	model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+	return model
 
 					
 
