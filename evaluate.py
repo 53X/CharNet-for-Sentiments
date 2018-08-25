6841b2155bd01df9912ba94419ce6e456729eee3
53X <mirage_1997@mail.ru>
2018-08-25 22:36:51 +0530

    Final commit

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,9 @@
 import keras
 from keras.callbacks import LearningRateScheduler, ModelCheckpoint, EarlyStopping
 import math
-
-
+from prep_data import structure_data
+from model import character_model
+from sklearn.model_selection import train_test_split 
 
 def step_decay(epoch):
 
@@ -21,7 +22,16 @@ modelcheckpoint = ModelCheckpoint('saved_model.h5', save_best_only=True,
 								  verbose=1)
 earlystopping = EarlyStopping(monitor='val_acc', mode='max', patience=5, verbose=1)
 
+data, labels = structure_data()
+x_train, x_test, y_train, y_test = train_test_split(data, labels, train_size=0.94,
+													test_size=0.06, stratify=labels)
 
+model = character_model(classification=4)
+
+model.fit(x_train, y_train, batch_size=128, epochs=200,
+		  callbacks=[modelcheckpoint, earlystopping, learning_rate_scheduler])
+
+print("ACCURACY ON TEST SET : ", model.evaluate(x_test, y_test))
 
 
 
--- model.py
+++ model.py
@@ -1,40 +1,44 @@
 import keras
-from keras.layers import Conv1D, MaxPooling1D, Dense, Input, Embedding, Dropout
+from keras.layers import Conv1D, MaxPooling1D, Dense, Input, Embedding, Dropout, Flatten
 from keras.optimizers import SGD
+from keras.initializers import RandomNormal
+from keras.models import Model
 
 
-def character_model(vocab_size=69, maxlen=1014, classification):
+def character_model(classification, vocab_size=69, maxlen=1014):
 
 	optimizer = SGD(lr=0.01, momentum=0.9)
 
-	input_layer = Input(shape=(maxlen, ), name='sentence input')
+	init = RandomNormal(mean=0.0, stddev=0.02)
+
+	input_layer = Input(shape=(maxlen, ), name='sentence_input')
 
 	embedding = Embedding(input_dim = vocab_size+1, output_dim = 70,
-						  name='character embedding')(input_layer)
+						  name='character_embedding')(input_layer)
 
 	conv_1 = Conv1D(filters=1024, kernel_size=7,  padding='valid',
-					activation='relu', name='first conv')(embedding)
+					activation='relu',kernel_initializer=init ,name='first_conv')(embedding)
 
-	pool_1 = MaxPooling1D(pool_size=3, padding='valid', name='first pool')(conv_1)
+	pool_1 = MaxPooling1D(pool_size=3, padding='valid', name='first_pool')(conv_1)
 
 	conv_2 = Conv1D(filters=1024, kernel_size=7,  padding='valid',
-					activation='relu', name='second conv')(pool_1)
+					activation='relu',kernel_initializer=init, name='second_conv')(pool_1)
 
-	pool_2 = MaxPooling1D(pool_size=3, padding='valid', name='second pool')(conv_2)
+	pool_2 = MaxPooling1D(pool_size=3, padding='valid', name='second_pool')(conv_2)
 
 	conv_3 = Conv1D(filters=1024, kernel_size=3,  padding='valid',
-					activation='relu', name='third conv')(pool_2)
+					activation='relu',kernel_initializer=init, name='third_conv')(pool_2)
 
 	conv_4 = Conv1D(filters=1024, kernel_size=3,  padding='valid',
-					activation='relu', name='fourth conv')(conv_3)
+					activation='relu',kernel_initializer=init, name='fourth_conv')(conv_3)
 
 	conv_5 = Conv1D(filters=1024, kernel_size=3,  padding='valid',
-					activation='relu', name='fifth conv')(conv_4)
+					activation='relu',kernel_initializer=init, name='fifth_conv')(conv_4)
 
 	conv_6 = Conv1D(filters=1024, kernel_size=3,  padding='valid',
-					activation='relu', name='sixth conv')(conv_5)
+					activation='relu',kernel_initializer=init, name='sixth_conv')(conv_5)
 
-	pool_6 = MaxPooling1D(pool_size=3, padding='valid', name='third pool')(conv_6)
+	pool_6 = MaxPooling1D(pool_size=3, padding='valid', name='third_pool')(conv_6)
 
 	flattened = Flatten()(pool_6)
 
